c1076fd1a26a8a6143ef0a41eaeeeb8be0aeaf19
Michael Ragusa <mragusa@infoblox.com>
2024-06-11 18:42:57 -0400

    basic framework file to build scripts using ibx-sdk

---
+++ ibx-sdk-framework.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+
+import getpass
+import sys
+import click
+from click_option_group import optgroup
+
+from ibx_sdk.logger.ibx_logger import init_logger, increase_log_level
+
+from ibx_sdk.nios.exceptions import WapiRequestException
+from ibx_sdk.nios.gift import Gift
+
+log = init_logger(
+    logfile_name="wapi.log",
+    logfile_mode="a",
+    console_log=True,
+    level="info",
+    max_size=100000,
+    num_logs=1,
+)
+
+wapi = Gift()
+
+help_text = """
+Edit as Needed
+"""
+
+
+@click.command(
+    help=help_text,
+    context_settings=dict(max_content_width=95, help_option_names=["-h", "--help"]),
+)
+@optgroup.group("Required Parameters")
+@optgroup.option("-g", "--grid-mgr", required=True, help="Infoblox Grid Manager")
+@optgroup.group("Optional Parameters")
+@optgroup.option(
+    "-u",
+    "--username",
+    default="admin",
+    show_default=True,
+    help="Infoblox admin username",
+)
+@optgroup.option(
+    "-w", "--wapi-ver", default="2.11", show_default=True, help="Infoblox WAPI version"
+)
+@optgroup.group("Logging Parameters")
+@optgroup.option("--debug", is_flag=True, help="enable verbose debug output")
+def main(grid_mgr: str, username: str, wapi_ver: str, debug: bool) -> None:
+    if debug:
+        increase_log_level()
+    wapi.grid_mgr = grid_mgr
+    wapi.wapi_ver = wapi_ver
+    password = getpass.getpass(f"Enter password for [{username}]: ")
+    try:
+        wapi.connect(username=username, password=password)
+    except WapiRequest as err:
+        log.error(err)
+        sys.exit(1)
+    else:
+        log.info("connected to Infoblox grid manager %s", wapi.grid_mgr)
+    try:
+        # Retrieve network view from Infoblox appliance
+        network_view = wapi.get("view")
+        print(network_view.json())
+    except WapiRequestException as err:
+        log.error(err)
+        sys.exit(1)
+
+    sys.exit()
+
+
+if __name__ == "__main__":
+    main()
