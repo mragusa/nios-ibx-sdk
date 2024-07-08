d34dfedc935205864197c8fccd92d1da1feee8f0
Michael Ragusa <mragusa@infoblox.com>
2024-07-08 09:04:51 -0400

    added additional options for configuring member

--- nios_preprovision.py
+++ nios_preprovision.py
@@ -43,11 +43,114 @@ Hardware Type: CP-V1405,CP-V2205,CP-V805,IB-100,IB-1410,IB-1415,IB-1420,IB-1425,
 )
 @optgroup.group("Required Parameters")
 @optgroup.option("-g", "--grid-mgr", required=True, help="Infoblox Grid Manager")
-@optgroup.group("Optional Parameters")
-@optgroup.option("-p", "--platform", type=click.Choice(['CISCO', 'IBVM', 'INFOBLOX', 'RIVERBED', 'VNIOS'], case_sensitive=True), help="Infoblox platform")
-@optgroup.option("-l", "--license", type=click.Choice(['cloudapi', 'dhcp', 'dns', 'dtc', 'enterprise', 'fireeye', 'ms_management', 'nios', 'rpz', 'sw_tp', 'tp_sub', 'vnios'], case_sensitive=True), help="License types the pre-provisioned member should have in order to join the Grid")
-@optgroup.option("-m", "--model", type=click.Choice(["CP-V1400","CP-V2200","CP-V800","IB-VM-100","IB-VM-1410","IB-VM-1420","IB-VM-2210","IB-VM-2220","IB-VM-4010","IB-VM-810","IB-VM-820","IB-VM-RSP","Rev1","Rev2"]), help="Hardware model")
-@optgroup.option("-t", "--type", type=click.Choice(["CP-V1405","CP-V2205","CP-V805","IB-100","IB-1410","IB-1415","IB-1420","IB-1425","IB-2210","IB-2215","IB-2220","IB-2225","IB-4010","IB-4015","IB-4020","IB-4025","IB-4030","IB-4030-10GE","IB-810","IB-815","IB-820","IB-825","IB-FLEX","IB-RSP2","IB-V1415","IB-V1425","IB-V2215","IB-V2225","IB-V4015","IB-V4025","IB-V815","IB-V825","IB-VNIOS","PT-1400","PT-1405","PT-2200","PT-2205","PT-4000","PT-4000-10GE"]), help="Hardware type")
+@optgroup.option(
+    "-p",
+    "--platform",
+    required=True,
+    type=click.Choice(
+        ["CISCO", "IBVM", "INFOBLOX", "RIVERBED", "VNIOS"], case_sensitive=True
+    ),
+    help="Infoblox platform",
+)
+@optgroup.option(
+    "-l",
+    "--licenses",
+    required=True,
+    type=click.Choice(
+        [
+            "cloudapi",
+            "dhcp",
+            "dns",
+            "dtc",
+            "enterprise",
+            "fireeye",
+            "ms_management",
+            "nios",
+            "rpz",
+            "sw_tp",
+            "tp_sub",
+            "vnios",
+        ],
+        case_sensitive=True,
+    ),
+    help="License types the pre-provisioned member should have in order to join the Grid",
+)
+@optgroup.option(
+    "-m",
+    "--model",
+    required=True,
+    type=click.Choice(
+        [
+            "CP-V1400",
+            "CP-V2200",
+            "CP-V800",
+            "IB-VM-100",
+            "IB-VM-1410",
+            "IB-VM-1420",
+            "IB-VM-2210",
+            "IB-VM-2220",
+            "IB-VM-4010",
+            "IB-VM-810",
+            "IB-VM-820",
+            "IB-VM-RSP",
+            "Rev1",
+            "Rev2",
+        ]
+    ),
+    help="Hardware model",
+)
+@optgroup.option(
+    "-t",
+    "--hwtype",
+    required=True,
+    type=click.Choice(
+        [
+            "CP-V1405",
+            "CP-V2205",
+            "CP-V805",
+            "IB-100",
+            "IB-1410",
+            "IB-1415",
+            "IB-1420",
+            "IB-1425",
+            "IB-2210",
+            "IB-2215",
+            "IB-2220",
+            "IB-2225",
+            "IB-4010",
+            "IB-4015",
+            "IB-4020",
+            "IB-4025",
+            "IB-4030",
+            "IB-4030-10GE",
+            "IB-810",
+            "IB-815",
+            "IB-820",
+            "IB-825",
+            "IB-FLEX",
+            "IB-RSP2",
+            "IB-V1415",
+            "IB-V1425",
+            "IB-V2215",
+            "IB-V2225",
+            "IB-V4015",
+            "IB-V4025",
+            "IB-V815",
+            "IB-V825",
+            "IB-VNIOS",
+            "PT-1400",
+            "PT-1405",
+            "PT-2200",
+            "PT-2205",
+            "PT-4000",
+            "PT-4000-10GE",
+        ]
+    ),
+    help="Hardware type",
+)
+@optgroup.option("-n", "--name", help="FQDN of Infoblox member")
+@optgroup.option("-i", "--ip", help="IP address of NIOS member")
+@optgroup.option("-m", "--mask", help="Subnet mask of NIOS member")
 @optgroup.option(
     "-u",
     "--username",
@@ -55,12 +158,24 @@ Hardware Type: CP-V1405,CP-V2205,CP-V805,IB-100,IB-1410,IB-1415,IB-1420,IB-1425,
     show_default=True,
     help="Infoblox admin username",
 )
+@optgroup.group("Optional Parameters")
+@optgroup.option("-ha", "--highavailablity", help="Enable HA ports")
 @optgroup.option(
     "-w", "--wapi-ver", default="2.11", show_default=True, help="Infoblox WAPI version"
 )
 @optgroup.group("Logging Parameters")
 @optgroup.option("--debug", is_flag=True, help="enable verbose debug output")
-def main(grid_mgr: str, username: str, wapi_ver: str, debug: bool) -> None:
+def main(
+    grid_mgr: str,
+    username: str,
+    wapi_ver: str,
+    debug: bool,
+    name: str,
+    model: str,
+    platform: str,
+    hwtype: str,
+    licenses: list,
+) -> None:
     if debug:
         increase_log_level()
     wapi.grid_mgr = grid_mgr
@@ -74,12 +189,26 @@ def main(grid_mgr: str, username: str, wapi_ver: str, debug: bool) -> None:
     else:
         log.info("connected to Infoblox grid manager %s", wapi.grid_mgr)
     try:
-        # Retrieve network view from Infoblox appliance
-        network_view = wapi.get("view")
-        print(network_view.json())
+        # Setup preprovisioning for Infoblox member
+        member_preprovision = wapi.post(
+            "member",
+            params={
+                "hostname": name,
+                "pre_provisioning": {
+                    "hardware_info": {"hwmodel": model, "hw_type": hwtype},
+                    "licenses": licenses,
+                },
+            },
+        )
     except WapiRequestException as err:
         log.error(err)
         sys.exit(1)
+    if member_preprovision.status_code != 201:
+        log.error("member preprovisioning failed: %s", member_preprovision.text)
+    else:
+        log.info(
+            "member preprovisioning was successful: %s", member_preprovision.json()
+        )
 
     sys.exit()
 
