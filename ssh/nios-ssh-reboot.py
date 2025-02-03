17278e354b5d83ab351549839a80819779f69331
Michael Ragusa <mragusa@users.noreply.github.com>
2025-02-03 15:17:07 -0500

    inital commit

---
+++ ssh/nios-ssh-reboot.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+from pexpect import pxssh
+import getpass
+
+import click
+from click_option_group import optgroup
+
+prompt = r".* Infoblox > "
+
+help_text = """
+Interact with Infoblox members via ssh to reboot or shutdown
+
+For more information:
+    https://docs.infoblox.com/space/nios85/35477821/Using+the+NIOS+CLI
+"""
+
+
+@click.command(
+    help=help_text,
+    context_settings=dict(max_content_width=95, help_option_names=["-h", "--help"]),
+)
+@optgroup.group("Required Parameters")
+@optgroup.option("-g", "--grid", required=True, help="Infoblox Grid Manager")
+@optgroup.option(
+    "-u", "--username", default="admin", required=True, help="Infoblox admin username"
+)
+@optgroup.group("Optional Parameters")
+@optgroup.option("-r", "--reboot", is_flag=True, help="Reboot Grid Member")
+@optgroup.option("-s", "--shutdown", is_flag=True, help="Shutdown Grid Member")
+def main(
+    grid: str, username: str, enable: bool, disable: bool, show: bool, statistics: bool
+) -> None:
+    if reboot:
+        command = "reboot"
+    if shutdown:
+        command = "shutdown"
+
+    try:
+        s = pxssh.pxssh()
+        hostname = grid
+        username = username
+        password = getpass.getpass("password: ")
+        s.PROMPT = prompt
+        s.login(hostname, username, password, auto_prompt_reset=False)
+        s.prompt()  # match the prompt
+        s.sendline(command)  # run a command
+        s.prompt()  # match the prompt
+        print(s.before.decode())  # print everything before the prompt.
+        s.logout()
+    except pxssh.ExceptionPxssh as e:
+        print("pxssh failed on login.")
+        print(e)
+
+
+if __name__ == "__main__":
+    main()
