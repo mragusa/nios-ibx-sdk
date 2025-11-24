6600cc2cc7f65bcb3843cced4e40df649cbff86f
Michael Ragusa <mragusa@users.noreply.github.com>
2025-11-23 20:38:46 -0500

    added dns functions. renamed old to origin

--- operations/nios_compare_grid_networks.py
+++ operations/nios_compare_grid_networks.py
@@ -18,7 +18,7 @@ log = init_logger(
     num_logs=1,
 )
 
-old_grid = Gift()
+origin_grid = Gift()
 new_grid = Gift()
 
 help_text = """
@@ -32,7 +32,7 @@ Basic Infoblox script to compare networks between Infoblox grids
 )
 @optgroup.group("Required Parameters")
 @optgroup.option("-g", "--new-grid-mgr", required=True, help="Infoblox Grid Manager")
-@optgroup.option("-o", "--old-grid-mgr", required=True, help="Infoblox Grid Manager")
+@optgroup.option("-o", "--origin-grid-mgr", required=True, help="Infoblox Grid Manager")
 @optgroup.group("Optional Parameters")
 @optgroup.option(
     "-u",
@@ -57,27 +57,27 @@ Basic Infoblox script to compare networks between Infoblox grids
     help="enable verbose debug output",
 )
 def main(
-    old_grid_mgr: str, new_grid_mgr: str, username: str, wapi_ver: str, debug: bool
+    origin_grid_mgr: str, new_grid_mgr: str, username: str, wapi_ver: str, debug: bool
 ) -> None:
     if debug:
         increase_log_level()
-    old_grid.grid_mgr = old_grid_mgr
-    old_grid.wapi_ver = wapi_ver
-    old_grid.timeout = 600
+    origin_grid.grid_mgr = origin_grid_mgr
+    origin_grid.wapi_ver = wapi_ver
+    origin_grid.timeout = 600
 
     new_grid.grid_mgr = new_grid_mgr
     new_grid.wapi_ver = wapi_ver
     new_grid.timeout = 600
     password = getpass.getpass(f"Enter password for [{username}]: ")
     try:
-        old_grid.connect(username=username, password=password)
+        origin_grid.connect(username=username, password=password)
     except WapiRequestException as err:
         log.error(err)
         sys.exit(1)
     else:
         if debug:
-            log.info(f"Connected to Infoblox grid manager {old_grid.grid_mgr}")
-        print(f"Connected to Infoblox grid manager {old_grid.grid_mgr}")
+            log.info(f"Connected to Infoblox grid manager {origin_grid.grid_mgr}")
+        print(f"Connected to Infoblox grid manager {origin_grid.grid_mgr}")
 
     try:
         new_grid.connect(username=username, password=password)
@@ -89,27 +89,50 @@ def main(
             log.info(f"Connected to Infoblox grid manager {new_grid.grid_mgr}")
         print(f"Connected to Infoblox grid manager {new_grid.grid_mgr}")
 
-    old_grid_networks = get_networks(old_grid, debug)
-    new_grid_networks = get_networks(new_grid, debug)
-    if old_grid_networks and new_grid_networks:
-        print("Origin Grid: ", len(old_grid_networks))
-        print("Destination Grid: ", len(new_grid_networks))
-        old_networks = {n["network"] for n in old_grid_networks}
-        new_networks = {n["network"] for n in new_grid_networks}
-        only_in_old = old_networks - new_networks
-        old_in_new = new_networks - old_networks
-        # in_both = old_networks & new_networks
-        print("Only in Origin: ", only_in_old)
-        print("Only in Destination: ", old_in_new)
-        # print("In Both: ", in_both)
+    nios_dns_objects = ["zone_auth", "view", "record:host", "record:a", "record:ptr"]
+    nios_network_objects = ["network", "networkcontainer"]
+    for n in nios_network_objects:
+        origin_grid_networks = get_networks(origin_grid, n, debug)
+        destination_grid_networks = get_networks(new_grid, n, debug)
+        if origin_grid_networks and destination_grid_networks:
+            print(f"{n} Differences Report")
+            print("Origin Grid: ", len(origin_grid_networks))
+            print("Destination Grid: ", len(destination_grid_networks))
+            origin_networks = {n["network"] for n in origin_grid_networks}
+            destination_networks = {n["network"] for n in destination_grid_networks}
+            only_in_origin = origin_networks - destination_networks
+            origin_in_destination = destination_networks - origin_networks
+            # in_both = origin_networks & destination_networks
+            print("Only in Origin: ", only_in_origin)
+            print("Only in Destination: ", origin_in_destination)
+            # print("In Both: ", in_both)
+    for d in nios_dns_objects:
+        origin_grid_dns = get_dns(origin_grid, d, debug)
+        destination_grid_dns = get_dns(new_grid, d, debug)
+        if origin_grid_dns and destination_grid_dns:
+            print(f"{d} Differences Report")
+            print("Origin Grid: ", len(origin_grid_dns))
+            print("Destination Grid: ", len(destination_grid_dns))
+            if d == "zone_auth":
+                origin_dns = {d["fqdn"] for d in origin_grid_dns}
+                destination_dns = {d["fqdn"] for d in destination_grid_dns}
+            else:
+                origin_dns = {d["name"] for d in origin_grid_dns}
+                destination_dns = {d["name"] for d in destination_grid_dns}
+            only_in_origin = origin_dns - destination_dns
+            only_in_destination = destination_dns - origin_dns
+            # in_both = origin_dns & destination_dns
+            print("Only in Origin: ", only_in_origin)
+            print("Only in Destination: ", only_in_destination)
+            # print("In Both", in_both)
     sys.exit()
 
 
-def get_networks(wapi, debug):
+def get_networks(wapi, net_obj, debug):
     try:
         # Retrieve networks from Infoblox appliance
         nios_networks = wapi.get(
-            "network",
+            net_obj,
             params={
                 "_max_results": 110000,
                 "_return_fields": ["network", "network_view", "comment"],
@@ -127,6 +150,34 @@ def get_networks(wapi, debug):
             if debug:
                 log.info(nios_networks.json())
             return nios_networks.json()
+    except WapiRequestException as err:
+        log.error(err)
+        sys.exit(1)
+
+
+def get_dns(wapi, dns_obj, debug):
+    if dns_obj == "zone_auth":
+        obj_return_field = ["fqdn", "comment"]
+    else:
+        obj_return_field = ["name", "comment"]
+    try:
+        # Retrieve dns objects from Infoblox appliance
+        nios_dns = wapi.get(
+            dns_obj,
+            params={"_max_results": 110000, "_return_fields": obj_return_field},
+        )
+        if nios_dns.status_code != 200:
+            if debug:
+                print(
+                    f"{nios_dns.status_code}: {nios_dns.json().get('code')}: {nios_dns.json().get('text')}"
+                )
+            log.error(
+                f"{nios_dns.status_code}: {nios_dns.json().get('code')}: {nios_dns.json().get('text')}"
+            )
+        else:
+            if debug:
+                log.info(nios_dns.json())
+            return nios_dns.json()
     except WapiRequestException as err:
         log.error(err)
         sys.exit(1)
