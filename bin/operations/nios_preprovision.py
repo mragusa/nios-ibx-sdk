1a3d704085d3bd3f78f7a947be47b109bbc84dc0
Michael Ragusa <mragusa@users.noreply.github.com>
2026-01-25 22:07:17 -0500

    Fixed WapiException line and updated HW models to reflect new X6 hardware line. Removed hwmodel as it has been depravated

--- bin/operations/nios_preprovision.py
+++ bin/operations/nios_preprovision.py
@@ -30,11 +30,9 @@ Platforms avaiable: CISCO, IBVM, INFOBLOX, RIVERBED, VNIOS
 
 License Types: cloud_api,dhcp,dns,dtc,enterprise,fireeye,ms_management,nios,rpz,sw_tp,tp_sub,vnios
 
-Hardware Model: CP-V1400,CP-V2200,CP-V800,IB-VM-100,IB-VM-1410,IB-VM-1420,IB-VM-2210,IB-VM-2220,IB-VM-4010,IB-VM-810,IB-VM-820,IB-VM-RSP,Rev1,Rev2
-
 Note that you cannot specify hwmodel for following hardware types: IB-FLEX, IB-V2215, IB-V1425, IB-V4025, IB-V4015, IB-V1415, IB-V815, IB-V825, IB-V2225, CP-V805, CP-V1405, CP-V2205, ‘IB-2215, IB-2225.
 
-Hardware Type: CP-V1405,CP-V2205,CP-V805,IB-100,IB-1410,IB-1415,IB-1420,IB-1425,IB-2210,IB-2215,IB-2220,IB-2225,IB-4010,IB-4015,IB-4020,IB-4025,IB-4030,IB-4030-10GE,IB-810,IB-815,IB-820,IB-825,IB-FLEX,IB-RSP2,IB-V1415,IB-V1425,IB-V2215,IB-V2225,IB-V4015,IB-V4025,IB-V815,IB-V825,IB-VNIOS,PT-1400,PT-1405,PT-2200,PT-2205,PT-4000,PT-4000-10GE
+Hardware Type: CP-V1405,CP-V2205,CP-V805,IB-1415,IB-1425,IB-1516,IB-1526,IB-2215,IB-2225,IB-2326,IB-4015,IB-4025,IB-4126,IB-815,IB-825,IB-926,IB-FLEX,IB-V1415,IB-V1425,IB-V1516,IB-V1526,IB-V2215,IB-V2225,IB-V2326,IB-V4015,IB-V4025,IB-V4126,IB-815,IB-V825,IB-V926
 """
 
 
@@ -78,29 +76,6 @@ Hardware Type: CP-V1405,CP-V2205,CP-V805,IB-100,IB-1410,IB-1415,IB-1420,IB-1425,
     help="License types the pre-provisioned member should have in order to join the Grid",
 )
 @optgroup.option(
-    "-m",
-    "--model",
-    type=click.Choice(
-        [
-            "CP-V1400",
-            "CP-V2200",
-            "CP-V800",
-            "IB-VM-100",
-            "IB-VM-1410",
-            "IB-VM-1420",
-            "IB-VM-2210",
-            "IB-VM-2220",
-            "IB-VM-4010",
-            "IB-VM-810",
-            "IB-VM-820",
-            "IB-VM-RSP",
-            "Rev1",
-            "Rev2",
-        ]
-    ),
-    help="Hardware model",
-)
-@optgroup.option(
     "-t",
     "--hwtype",
     type=click.Choice(
@@ -108,42 +83,33 @@ Hardware Type: CP-V1405,CP-V2205,CP-V805,IB-100,IB-1410,IB-1415,IB-1420,IB-1425,
             "CP-V1405",
             "CP-V2205",
             "CP-V805",
-            "IB-100",
-            "IB-1410",
             "IB-1415",
-            "IB-1420",
             "IB-1425",
-            "IB-2210",
+            "IB-1516",
+            "IB-1526",
             "IB-2215",
-            "IB-2220",
             "IB-2225",
-            "IB-4010",
+            "IB-2326",
             "IB-4015",
-            "IB-4020",
             "IB-4025",
-            "IB-4030",
-            "IB-4030-10GE",
-            "IB-810",
+            "IB-4126",
             "IB-815",
-            "IB-820",
             "IB-825",
+            "IB-926",
             "IB-FLEX",
-            "IB-RSP2",
             "IB-V1415",
             "IB-V1425",
+            "IB-V1516",
+            "IB-V1526",
             "IB-V2215",
             "IB-V2225",
+            "IB-V2326",
             "IB-V4015",
             "IB-V4025",
+            "IB-V4126",
             "IB-V815",
             "IB-V825",
-            "IB-VNIOS",
-            "PT-1400",
-            "PT-1405",
-            "PT-2200",
-            "PT-2205",
-            "PT-4000",
-            "PT-4000-10GE",
+            "IB-V926",
         ]
     ),
     help="Hardware type",
@@ -184,7 +150,6 @@ def main(
     vip: str,
     subnetmask: str,
     gateway: str,
-    model: str,
     platform: str,
     hwtype: str,
     licenses: list,
@@ -206,7 +171,7 @@ def main(
     password = getpass.getpass(f"Enter password for [{username}]: ")
     try:
         wapi.connect(username=username, password=password)
-    except WapiRequest as err:
+    except WapiRequestException as err:
         log.error(err)
         sys.exit(1)
     else:
@@ -291,14 +256,8 @@ def main(
     # utilize member reference from creation
     new_member = member_creation.json()
 
-    if model:
-        provision = {
-            "pre_provisioning": {
-                "hardware_info": [{"hwmodel": model}],
-                "licenses": licenses,
-            }
-        }
-
+    # Assign hardware type to new json object for updating new member
+    provision = {}
     if hwtype:
         provision = {
             "pre_provisioning": {
