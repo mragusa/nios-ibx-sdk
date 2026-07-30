724b14d3dbf3bf7b294fbdd954ead2255e85da6e
Michael Ragusa <mragusa@users.noreply.github.com>
2026-07-30 21:39:03 +0700

    fix: moved functions before main above click arguements to adhere to pip standards

--- src/nios-ibx-sdk/bin/operations/nios_a.py
+++ src/nios-ibx-sdk/bin/operations/nios_a.py
@@ -39,93 +39,6 @@ TTL defaults to 600 seconds for new records and 5 seconds for IP address updates
 current_time = date.today()
 
 
-@click.command(
-    help=help_text,
-    context_settings=dict(max_content_width=95, help_option_names=["-h", "--help"]),
-)
-@optgroup.group("Required Parameters")
-@optgroup.option("-g", "--grid-mgr", required=True, help="Infoblox Grid Manager")
-@optgroup.option("-f", "--fqdn", required=True, show_default=True, help="FQDN")
-@optgroup.option("-i", "--ip", required=True, show_default=True, help="IP address")
-@optgroup.group("Operational Parameters")
-@optgroup.option("--add", is_flag=True, default=False, help="Add A record")
-@optgroup.option("--delete", is_flag=True, default=False, help="Delete A record")
-@optgroup.option("--update", is_flag=True, default=False, help="Update A record")
-@optgroup.group("Optional Parameters")
-@optgroup.option(
-    "-u",
-    "--username",
-    default="admin",
-    show_default=True,
-    help="Infoblox admin username",
-)
-@optgroup.option(
-    "-w", "--wapi-ver", default="2.13", show_default=True, help="Infoblox WAPI version"
-)
-@optgroup.option("-t", "--ttl", show_default=True, default=600, help="TTL in seconds")
-@optgroup.option(
-    "-d",
-    "--disable",
-    is_flag=True,
-    default=False,
-    show_default=True,
-    help="Disable record",
-)
-@optgroup.option(
-    "-c",
-    "--comment",
-    show_default=True,
-    default=current_time,
-    help="Comment for record: default is creation date",
-)
-@optgroup.option("-v", "--view", show_default=True, default="default", help="DNS view")
-@optgroup.group("Update Parameters")
-@optgroup.option("--newname", help="New Hostname")
-@optgroup.option("--newip", help="New IP Address")
-@optgroup.option("--newttl", show_default=True, default=5, help="New TTL")
-@optgroup.group("Logging Parameters")
-@optgroup.option(
-    "--debug", is_flag=True, default=False, help="enable verbose debug output"
-)
-def main(
-    grid_mgr: str,
-    add: bool,
-    delete: bool,
-    update: bool,
-    username: str,
-    wapi_ver: str,
-    debug: bool,
-    fqdn: str,
-    ip: str,
-    ttl: int,
-    disable: bool,
-    comment: str,
-    view: str,
-    newname: str,
-    newip: str,
-    newttl: int,
-) -> None:
-    if debug:
-        increase_log_level()
-    wapi.grid_mgr = grid_mgr
-    wapi.wapi_ver = wapi_ver
-    password = getpass.getpass(f"Enter password for [{username}]: ")
-    try:
-        wapi.connect(username=username, password=password)
-    except WapiRequestException as err:
-        log.error(err)
-        sys.exit(1)
-    else:
-        log.info("connected to Infoblox grid manager %s", wapi.grid_mgr)
-    if add:
-        add_arecord(fqdn, ip, comment, disable, ttl)
-    if delete:
-        del_arecord(fqdn, ip, view)
-    if update:
-        update_arecord(fqdn, ip, view, newip, newname, newttl)
-    sys.exit()
-
-
 def add_arecord(fqdn, ip, comment, disable, ttl):
     print(f"Creating {fqdn} {ip} {comment} Disabled: {disable}")
     log.info(f"Creating {fqdn} {ip} {comment} Disabled: {disable}")
@@ -216,6 +129,93 @@ def update_arecord(fqdn, ip, view, newip, newname, newttl):
     except WapiRequestException as err:
         log.error(err)
         sys.exit(1)
+
+
+@click.command(
+    help=help_text,
+    context_settings=dict(max_content_width=95, help_option_names=["-h", "--help"]),
+)
+@optgroup.group("Required Parameters")
+@optgroup.option("-g", "--grid-mgr", required=True, help="Infoblox Grid Manager")
+@optgroup.option("-f", "--fqdn", required=True, show_default=True, help="FQDN")
+@optgroup.option("-i", "--ip", required=True, show_default=True, help="IP address")
+@optgroup.group("Operational Parameters")
+@optgroup.option("--add", is_flag=True, default=False, help="Add A record")
+@optgroup.option("--delete", is_flag=True, default=False, help="Delete A record")
+@optgroup.option("--update", is_flag=True, default=False, help="Update A record")
+@optgroup.group("Optional Parameters")
+@optgroup.option(
+    "-u",
+    "--username",
+    default="admin",
+    show_default=True,
+    help="Infoblox admin username",
+)
+@optgroup.option(
+    "-w", "--wapi-ver", default="2.13", show_default=True, help="Infoblox WAPI version"
+)
+@optgroup.option("-t", "--ttl", show_default=True, default=600, help="TTL in seconds")
+@optgroup.option(
+    "-d",
+    "--disable",
+    is_flag=True,
+    default=False,
+    show_default=True,
+    help="Disable record",
+)
+@optgroup.option(
+    "-c",
+    "--comment",
+    show_default=True,
+    default=current_time,
+    help="Comment for record: default is creation date",
+)
+@optgroup.option("-v", "--view", show_default=True, default="default", help="DNS view")
+@optgroup.group("Update Parameters")
+@optgroup.option("--newname", help="New Hostname")
+@optgroup.option("--newip", help="New IP Address")
+@optgroup.option("--newttl", show_default=True, default=5, help="New TTL")
+@optgroup.group("Logging Parameters")
+@optgroup.option(
+    "--debug", is_flag=True, default=False, help="enable verbose debug output"
+)
+def main(
+    grid_mgr: str,
+    add: bool,
+    delete: bool,
+    update: bool,
+    username: str,
+    wapi_ver: str,
+    debug: bool,
+    fqdn: str,
+    ip: str,
+    ttl: int,
+    disable: bool,
+    comment: str,
+    view: str,
+    newname: str,
+    newip: str,
+    newttl: int,
+) -> None:
+    if debug:
+        increase_log_level()
+    wapi.grid_mgr = grid_mgr
+    wapi.wapi_ver = wapi_ver
+    password = getpass.getpass(f"Enter password for [{username}]: ")
+    try:
+        wapi.connect(username=username, password=password)
+    except WapiRequestException as err:
+        log.error(err)
+        sys.exit(1)
+    else:
+        log.info("connected to Infoblox grid manager %s", wapi.grid_mgr)
+    if add:
+        add_arecord(fqdn, ip, comment, disable, ttl)
+    if delete:
+        del_arecord(fqdn, ip, view)
+    if update:
+        update_arecord(fqdn, ip, view, newip, newname, newttl)
+    sys.exit()
 
 
 if __name__ == "__main__":
